e322854c617cfd3c7bcb5f082a4480398b355f69
Sachin <sachinkumar052@gmail.com>
2022-03-25 18:56:54 +0530

    New Changes

--- app/src/main/java/com/example/FlowApp.kt
+++ app/src/main/java/com/example/FlowApp.kt
@@ -1,11 +1,14 @@
 package com.example
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class FlowApp:Application() {
     override fun onCreate() {
         super.onCreate()
+        Log.e("the","ashdkjffad")
+        Log.e("the","ashdkjffad")
     }
 }
